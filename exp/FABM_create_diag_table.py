ae1147d0f51ada931fb60780ac7c8c9246bbd01a
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2026-07-08 15:53:02 +0100

    update FABM preprocessing scripts

--- exp/FABM_create_diag_table.py
+++ exp/FABM_create_diag_table.py
@@ -1,61 +1,100 @@
 #!/usr/bin/env python
 
-# This script reads a fabm.yaml file and writes out entries to stdout that can be pasted into MOM's diag_table.
-# It must be run from the root directory of a MOM setup.
-# Requirements:
-# - the FABM-Python driver (pyfabm) must have been built and installed.
+"""This script reads a ``fabm.yaml`` file and writes out entries to stdout
+that can be pasted into MOM's ``diag_table``.
 
-from __future__ import print_function
+It must be pointed to the root directory of a MOM setup.
+
+To use this script, pyfabm (https://fabm.net/python) must be installed.
+If you intend to use custom biogeochemical models, pyfabm needs to be built
+with those models included. See https://fabm.net/python for details.
+"""
 
 import sys
-import os.path
 import argparse
+import os
+from pathlib import Path
+import logging
 
 try:
     import pyfabm
 except ImportError:
-    print('Unable to load pyfabm. See https://github.com/fabm-model/code/wiki/python.')
+    print("Unable to load pyfabm. See https://fabm.net/python.")
     sys.exit(1)
 
+logging.basicConfig(level=logging.INFO)
+pyfabm.logger = logging.getLogger("pyfabm")
+
 parser = argparse.ArgumentParser()
-parser.add_argument('expdir', help='path to experiment directory', nargs='?', default='.')
-parser.add_argument('--file_name', help='name of output file', default='ocean_fabm')
-parser.add_argument('--average', help='whether to time-average FABM fields', action='store_true', default=False)
-parser.add_argument('--precision', type=int, help='precision of FABM outputs (1: double precision, 2: float, 4: packed 16 bit integers)', default=2)
-parser.add_argument('--show_hidden', help='whether to include hidden FABM fields (entries will commented out)', action='store_true', default=False)
+parser.add_argument(
+    "expdir",
+    help="path to experiment directory",
+    nargs="?",
+    type=Path,
+    default=Path("."),
+)
+parser.add_argument("--file_name", help="name of output file", default="ocean_fabm")
+parser.add_argument(
+    "--average",
+    help="whether to time-average FABM fields",
+    action="store_true",
+    default=False,
+)
+parser.add_argument(
+    "--precision",
+    type=int,
+    help="precision of FABM outputs (1: double precision, 2: float, 4: packed 16 bit integers)",
+    default=2,
+)
+parser.add_argument(
+    "--show_hidden",
+    help="whether to include hidden FABM fields (entries will commented out)",
+    action="store_true",
+    default=False,
+)
 args = parser.parse_args()
 
-model = pyfabm.Model(os.path.join(args.expdir, 'fabm.yaml'))
+model = pyfabm.Model(os.fspath(args.expdir / "fabm.yaml"))
 
-print('#')
-print('## FABM')
-print('# (variables that are not part of FABM\'s default output are commented out)')
-print('#')
-print('"ocean_model","geolat_t","geolat_t","%s","all",.false.,"none",%i' % (args.file_name, args.precision))
-print('"ocean_model","geolon_t","geolon_t","%s","all",.false.,"none",%i' % (args.file_name, args.precision))
+print("#")
+print("## FABM")
+print("# (variables that are not part of FABM's default output are commented out)")
+print("#")
+print(
+    f'"ocean_model","geolat_t","geolat_t","{args.file_name}",'
+    f'"all",.false.,"none",{args.precision}'
+)
+print(
+    f'"ocean_model","geolon_t","geolon_t","{args.file_name}",'
+    f'"all",.false.,"none",{args.precision}'
+)
 
-def writeVariable(variable, output=None, name=None):
+
+def writeVariable(variable: pyfabm.Variable, output=None, name=None):
     if output is None:
         output = variable.output
     if name is None:
         name = variable.output_name
     if not (output or args.show_hidden):
         return
-    prefix = '' if output else '#'
-    print('%s"ocean_model","%s","%s","%s","all",%s,"none",%i' % (prefix, name, name, args.file_name, '.true.' if args.average else '.false.', args.precision))
+    prefix = "" if output else "#"
+    print(
+        f'{prefix}"ocean_model","{name}","{name}","{args.file_name}","all",{".true." if args.average else ".false."},"none",{args.precision}'
+    )
 
-print('#')
-print('# State variables')
-print('#')
+
+print("#")
+print("# State variables")
+print("#")
 for variable in model.state_variables:
     writeVariable(variable)
-print('#')
-print('# Diagnostic variables')
-print('#')
+print("#")
+print("# Diagnostic variables")
+print("#")
 for variable in model.diagnostic_variables:
     writeVariable(variable)
-print('#')
-print('# Conserved quantities')
-print('#')
+print("#")
+print("# Conserved quantities")
+print("#")
 for variable in model.conserved_quantities:
-    writeVariable(variable, name=variable.name+'_global_int', output=True)
+    writeVariable(variable, name=variable.name + "_global_int", output=True)
--- exp/FABM_create_initial_condition.py
+++ exp/FABM_create_initial_condition.py
@@ -1,25 +1,43 @@
 #!/usr/bin/env python
 
-# This script reads a fabm.yaml file, and writes out MOM restart files for all FABM state variables.
-# It must be run from the INPUT directory of a MOM setup.
-# Requirements:
-# - the FABM-Python driver (pyfabm) must have been built and installed.
-# - the netCDF Operator toolkit (http://nco.sourceforge.net)
+"""Create MOM restart files for FABM state variables.
 
-from __future__ import print_function
+This script reads ``fabm.yaml`` and writes MOM restart files for all FABM
+state variables. It must be pointed to the experiment directory that contains
+the ``INPUT`` subdirectory used by MOM.
+
+To use this script, pyfabm (https://fabm.net/python) must be installed.
+If you intend to use custom biogeochemical models, pyfabm needs to be built
+with those models included. See https://fabm.net/python for details.
+"""
 
 import argparse
 import sys
-import os.path
+import os
+from pathlib import Path
+from typing import Mapping, Optional, Iterable, Tuple, Union
+
 import netCDF4
 
 try:
     import pyfabm
 except ImportError:
-    print('Unable to load pyfabm. See https://github.com/fabm-model/fabm/wiki/python.')
+    print("Unable to load pyfabm. See https://fabm.net/python.")
     sys.exit(1)
 
-def copy_dimensions(ncin, ncout, dimensions):
+
+def copy_dimensions(
+    ncin: netCDF4.Dataset,
+    ncout: netCDF4.Dataset,
+    dimensions: Iterable[str],
+):
+    """Copy selected dimensions and corresponding coordinate variables.
+
+    Args:
+        ncin: Source NetCDF dataset.
+        ncout: Destination NetCDF dataset.
+        dimensions: Names of dimensions to copy from ``ncin`` to ``ncout``.
+    """
     for name in dimensions:
         ncout.createDimension(name, ncin.dimensions[name].size)
         if name in ncin.variables:
@@ -29,40 +47,67 @@ def copy_dimensions(ncin, ncout, dimensions):
             for attname in ncvar_in.ncattrs():
                 setattr(ncvar, attname, getattr(ncvar_in, attname))
 
-def run(expdir, rho=1025., custom={}):
-    model = pyfabm.Model(os.path.join(expdir, 'fabm.yaml'))
 
-    nc_ts = netCDF4.Dataset(os.path.join(expdir, 'INPUT/ocean_temp_salt.res.nc'), 'r')
-    dimensions_3d = nc_ts.variables['temp'].dimensions
+def run(
+    expdir: Union[os.PathLike, str],
+    rho: float = 1025.0,
+    custom: Optional[Mapping[str, Tuple[str, str]]] = {},
+) -> None:
+    """Generate FABM restart files for a MOM experiment.
+
+    Args:
+        expdir: Path to the experiment directory containing ``fabm.yaml`` and
+            the ``INPUT`` folder.
+        rho: Reference seawater density (kg m-3) used to convert FABM interior
+            tracers from per-volume to per-mass units.
+        custom: Optional mapping from FABM output variable name to
+            ``(path, expression)``. The expression is evaluated against the
+            netCDF variables from ``path`` to construct an initial field.
+    """
+    exp_path = Path(expdir)
+    model = pyfabm.Model(os.fspath(exp_path / "fabm.yaml"))
+
+    input_dir = exp_path / "INPUT"
+    template_file = input_dir / "ocean_temp_salt.res.nc"
+    print(f"Using template file: {template_file}")
+    nc_ts = netCDF4.Dataset(template_file)
+    dimensions_3d = nc_ts.variables["temp"].dimensions
     dimensions_2d = dimensions_3d[:-3] + dimensions_3d[-2:]
-    dtype = nc_ts.variables['temp'].dtype
+    dtype = nc_ts.variables["temp"].dtype
 
-    print('Writing 3D restart file: ocean_fabm.res.nc')
-    with netCDF4.Dataset(os.path.join(expdir, 'INPUT/ocean_fabm.res.nc'), 'w') as nc3d:
+    state_3d_file = input_dir / "ocean_fabm.res.nc"
+    print(f"Writing 3D restart file: {state_3d_file}")
+    with netCDF4.Dataset(state_3d_file, "w") as nc3d:
         copy_dimensions(nc_ts, nc3d, dimensions_3d)
         for variable in model.interior_state_variables:
-            print('  %s: ' % variable.output_name, end='')
+            print(f"  {variable.output_name}: ", end="")
             ncvar = nc3d.createVariable(variable.output_name, dtype, dimensions_3d)
-            ncvar.units = '%s m3 kg-1' % variable.units
+            ncvar.units = f"{variable.units} m3 kg-1"
             ncvar.long_name = variable.long_path
             value = variable.value
             if variable.output_name in custom:
                 path, name = custom[variable.output_name]
                 with netCDF4.Dataset(path) as nc:
-                    value = eval(name, dict([(n, v[...]) for (n, v) in nc.variables.items()]))
-                print('[%s read from %s, mean = %s, min = %s, max = %s]' % (name, path, value.mean(), value.min(), value.max()))
+                    value = eval(name, {n: v[...] for (n, v) in nc.variables.items()})
+                print(
+                    f"[{name} read from {path},"
+                    f" mean = {value.mean()}, min = {value.min()}, max = {value.max()}]"
+                )
             else:
                 print(value)
 
             # Divide the initial value of interior state variable by density (default: 1025 kg m-3),
             # as MOM tracks tracer per seawater mass, rather than tracer per seawater volume as in FABM.
-            ncvar[...] = value/rho
+            ncvar[...] = value / rho
 
-    print('Writing 2D restart file: ocean_fabm_2d.res.nc')
-    with netCDF4.Dataset(os.path.join(expdir, 'INPUT/ocean_fabm_2d.res.nc'), 'w') as nc2d:
+    state_2d_file = input_dir / "ocean_fabm_2d.res.nc"
+    print(f"Writing 2D restart file: {state_2d_file}")
+    with netCDF4.Dataset(state_2d_file, "w") as nc2d:
         copy_dimensions(nc_ts, nc2d, dimensions_2d)
-        for variable in tuple(model.bottom_state_variables)+tuple(model.surface_state_variables):
-            print('  %s: ' % variable.output_name, end='')
+        for variable in tuple(model.bottom_state_variables) + tuple(
+            model.surface_state_variables
+        ):
+            print(f"  {variable.output_name}: ", end="")
             ncvar = nc2d.createVariable(variable.output_name, dtype, dimensions_2d)
             ncvar.units = variable.units
             ncvar.long_name = variable.long_path
@@ -70,27 +115,46 @@ def run(expdir, rho=1025., custom={}):
             if variable.output_name in custom:
                 path, name = custom[variable.output_name]
                 with netCDF4.Dataset(path) as nc:
-                    value = eval(name, dict([(n, v[...]) for (n, v) in nc.variables.items()]))
-                print('[%s read from %s, mean = %s, min = %s, max = %s]' % (name, path, value.mean(), value.min(), value.max()))
+                    value = eval(
+                        name, dict([(n, v[...]) for (n, v) in nc.variables.items()])
+                    )
+                print(
+                    f"[{name} read from {path},"
+                    f" mean = {value.mean()}, min = {value.min()}, max = {value.max()}]"
+                )
             else:
                 print(value)
             ncvar[...] = value
 
-    print('Writing chlorophyll restart (all 0): ocean_chl.res.nc')
-    with netCDF4.Dataset(os.path.join(expdir, 'INPUT/ocean_chl.res.nc'), 'w') as ncchl:
+    chl_file = input_dir / "ocean_chl.res.nc"
+    print(f"Writing chlorophyll restart (all 0): {chl_file}")
+    with netCDF4.Dataset(chl_file, "w") as ncchl:
         copy_dimensions(nc_ts, ncchl, dimensions_3d)
-        ncvar = ncchl.createVariable('chl', dtype, dimensions_3d)
-        ncvar[...] = 0.
+        ncvar = ncchl.createVariable("chl", dtype, dimensions_3d)
+        ncvar[...] = 0.0
 
-    print('Writing irradiance restart (all 0): ocean_irr.res.nc')
-    with netCDF4.Dataset(os.path.join(expdir, 'INPUT/ocean_irr.res.nc'), 'w') as ncirr:
+    irr_file = input_dir / "ocean_irr.res.nc"
+    print(f"Writing irradiance restart (all 0): {irr_file}")
+    with netCDF4.Dataset(irr_file, "w") as ncirr:
         copy_dimensions(nc_ts, ncirr, dimensions_3d)
-        ncvar = ncirr.createVariable('irr', dtype, dimensions_3d)
-        ncvar[...] = 0.
+        ncvar = ncirr.createVariable("irr", dtype, dimensions_3d)
+        ncvar[...] = 0.0
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('expdir', help='path to experiment directory', nargs='?', default='.')
-    parser.add_argument('--rho', help='reference density of sea water (kg m-3)', type=float, default=1025.)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(usage=__doc__)
+    parser.add_argument(
+        "expdir",
+        help="path to experiment directory",
+        nargs="?",
+        type=Path,
+        default=Path("."),
+    )
+    parser.add_argument(
+        "--rho",
+        help="reference density of sea water (kg m-3)",
+        type=float,
+        default=1025.0,
+    )
     args = parser.parse_args()
     run(args.expdir, args.rho)
